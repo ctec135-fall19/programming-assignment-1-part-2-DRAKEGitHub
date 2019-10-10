9814fcdc8c051dc68ce14851f16a8c654c0b18b7
Allison Drake <allisonnicoledrake@gmail.com>
2019-10-10 07:58:32 -0700

    NOT COMPLETE: pushing to save in GitHub for class help

---
+++ Prob2/Program.cs
@@ -0,0 +1,65 @@
+﻿/*
+Author: Allison Drake
+Date: 10/09/2019
+CTEC 135: Microsoft Software Development with C#
+
+Module 2, Programming Assignment 1 Part 2: 2 Coding Projects, Problem 2
+
+Create a New Project called Prob2. 
+Print out the numbers 1-5 using loops:
+1. print 1-5 in single line for loop
+2. print 1-5 on single line using while loop
+3. print 1-5 using single line with do/while loop
+Use Write() instead of WriteLine() in loop body and end with blank line.
+
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prob2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        // Inputs: myInts 1-5, int
+        // Outputs: Prints Loop Type and three versions of 1 2 3 4 5
+        // Behavior: loops three ways to print 1-5
+        {
+            #region for loop print 1-5 and space on single line
+            Console.WriteLine("For loop that prints 1-5:");
+            for (int i=1; i<6; i++)            {
+                Console.Write("{0} ",i);
+            }
+            Console.WriteLine();
+            #endregion
+
+
+            #region while loop print 1-5 and space on single line
+            Console.WriteLine("While loop that prints 1-5:");
+            int[] myInts = { 1, 2, 3, 4, 5 };
+            int count = 0; 
+            while(count < myInts.Length)
+            {
+                Console.Write("{0} ", myInts[count]);
+                count++; 
+            }
+            Console.WriteLine();
+            #endregion
+
+            #region do/while, print 1-5 and space on single line
+            Console.WriteLine("Do/While that prints 1-5:");
+            count = 0;
+            do
+            {
+                Console.Write("{0} ", myInts[count]);
+                count++;
+            } while (count < myInts.Length);
+            Console.WriteLine();
+            #endregion
+        }
+    }
+}
---
+++ Prob3/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+Author: Allison Drake
+Date: 10/09/2019
+CTEC 135: Microsoft Software Development with C#
+
+Module 2, Programming Assignment 1 Part 2: 2 Coding Projects, Problem 3
+
+Create a New Project called Prob3
+Create a program that prints out actions based upon decision table.
+Create methods for each action that contains print statement with text in Ouput
+Methods are same level as Main()
+Region 1 inside Main() is 3 nested loops to execute conditions
+print a line in the innermost loop stating the conditions
+Use if statements to call methods and print possible actions
+Region 2 Inside Main() Use binary table method in a single for loop for 0 to 7
+use a switch statement in the body with iteration variable as its value
+code in each case will call the appropriate methods based on number
+
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prob3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region ONE: Nested loops
+
+            #endregion
+
+            #region TWO: Switch in a loop
+
+            #endregion
+
+        }
+    }
+}
